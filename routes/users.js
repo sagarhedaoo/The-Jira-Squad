fce5acfc85287600eb062853c65925535d6b5764
Khusboo Patel <kkp5601@gmail.com>
2023-03-06 23:24:02 -0500

    Changed users.js in route

--- routes/users.js
+++ routes/users.js
@@ -19,3 +19,65 @@ router
 	res.render('users/signup');
        
     })
+    .post(async (req, res) => {
+        try {
+
+            if (req.session.user) {
+                res.status(400).json("You are already loggedIn why sign up again?????");
+
+            }
+            else {
+                const { username,nickname,password} = req.body;
+	try
+	{
+		if (!username) throw `You must provide a username`;
+		
+		if (!nickname) throw `You must provide a nickname`;
+		
+		if (!password[0]) throw `You must provide a password`;
+		
+		if (!password[1]) throw `You must provide a confirm password`;
+		
+		if(password[0]!=password[1]) throw `Password don't match`;
+
+        //error check for password
+
+        if(password.length === 0)
+        {
+        throw "Error : Please enter the password";
+        }
+        else if(password.length<3)
+        {	
+        throw "Error : Password must be atleast of 3 characters";
+        }
+        else if(password.length>20)
+        {
+        throw "Error : Password must not be more than 20 characters";
+        }
+        else
+        {
+            const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+        if((password.match("[a-zA-Z0-9]" === null) || (password.match(regex) === null)))
+        {
+            throw `Error : Password must contain atleast one capital letter, a number and a special character`;
+        }
+        }	
+        const hash = await bcrypt.hash(password[0], saltRounds);
+		const newUser = await userData.createUser(username,hash,nickname);
+
+		loggedOrNot = newUser._id.toHexString(); 
+
+        return res.redirect('/homePage');
+    }
+    catch (e) {
+        console.log(e)
+        if (e == "Error: A name, username and password is required for signup") { res.status(400).json(e) }
+        if (e == 'Error: Insertion failed') { res.status(500).json(e) }
+        //if (e == 'Error: Password must be at least 6 characters long and contain at least one lowercase letter, one uppercase letter, one number, and one special character.') { res.status(400).json(e) }
+        if(e == "Error: Invalid Input.") { res.status(400).json(e)}
+        else {
+            res.status(400).json(e)
+        }
+    }}}
+});
