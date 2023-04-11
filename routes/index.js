ddf00b70ed883133ac6e638e197b51a5ff05df26
Khusboo Patel <kkp5601@gmail.com>
2023-04-10 22:08:32 -0400

    Updated index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
-const userRoutes = require('./user');
+const path = require('path');
 
-const constructorMethod = (app) => {
-  app.use("/",userRoutes);
-  app.use('*', (req, res) => {
-    res.sendStatus(404);
-  });
+const userRoutes = require("./users");
+//const postRoutes = require("./posts");
+//const commentRoutes = require("./comments");
+//const reportRoutes = require("./reports");
+const homePageRoutes = require("./homePage");
+
+const constructorMethod = app => 
+{
+    app.use("/users", userRoutes);
+    //app.use("/posts", postRoutes);
+    //app.use("/comments", commentRoutes);
+    //app.use("/reports", reportRoutes);
+    app.use("/homePage", homePageRoutes);
+    
+    app.get('/', (req, res) => 
+    {
+        res.redirect('http://localhost:3000/homePage');
+    });
+
+    app.use("*", (req, res) => 
+    {
+        res.sendStatus(404);
+    });
 };
 
 module.exports = constructorMethod;
