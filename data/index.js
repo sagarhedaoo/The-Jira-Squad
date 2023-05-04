7592f928eb71372c5852e8b9f5ebba39b365647b
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-05-04 18:07:34 -0400

    *

--- data/index.js
+++
@@ -1,9 +0,0 @@
-const userData = require("./users");
-const tasksData = require("./tasks");
-const photoData = require("./photos");
-
-module.exports = {
-  users: userData,
-  tasks: tasksData,
-  photos: photoData,
-};
